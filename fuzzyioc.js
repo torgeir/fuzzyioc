a563b2828b50c2b7ddaa46e7278f2cb5af486567
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-05-30 17:02:18 +0200

    Tests pass.

--- fuzzyioc.js
+++ fuzzyioc.js
@@ -1,81 +1,246 @@
 var _ = require('lodash');
 var astw = require('astw');
-var deparse = require('escodegen').generate;
 
-/*
-MemberExpression
-  object.type = 'Identifier'
-  object.name = 'arg'
-  les property
+module.exports = function () {
 
-CallExpressions
-*/
+  var types = [];
 
-module.exports = input => {
+  var typesByProperty = { members: {}, methods: {} };
 
-  var sourceString = (typeof input == 'function')
-    ? input.toString()
-    : input;
+  /**
+   * Fuzzy ioc container. Satisfies dependencies for types by ducktyping against registered types.
+   */
+  function fuzzyioc (Type) {
+    var usages = fuzzyioc.parseUsages(Type);
+    var satisfiersPerDependency = satisfyUsages(usages);
 
-  var nodes = [];
+    var nodes = lookupNodes(Type);
+    var dependencyNames = findDependencies(nodes, Type);
 
-  var walk = astw(sourceString);
-  walk(node => nodes.push(node));
+    var dependencies = _.map(dependencyNames, dependency => satisfiersPerDependency[dependency][0]);
+    var dependencyInstances = _.map(dependencies, Dependency => fuzzyioc(Dependency));
 
-  var deps = {};
-
-  function initDependency (dep) {
-    deps[dep] = {
-      properties: [], // variable
-      functions: []   // { name, args }
-    };
+    return newInstance(Type, dependencyInstances);
   }
 
-  var reversedNodes = nodes.reverse();
+  /**
+   * Returns types registered with fuzzyioc.
+   */
+  fuzzyioc.types = () => types.slice();
 
-  _.each(reversedNodes, node => {
+  /**
+   * Registers a Type in fuzzyioc. Indexes types by members and methods available on the Type.
+   */
+  fuzzyioc.register = Type => {
+    types.push(Type);
 
-    switch (node.type) {
-      case 'FunctionExpression':
-        _.map(node.params, param => initDependency(param.name));
-      break;
+    var properties = parseProperties(Type);
+
+    // index members
+    _.each(properties.members, member => {
+      var members = typesByProperty.members[member] = (typesByProperty.members[member] || []);
+      members.push(Type);
+    });
+
+    // index methods
+    _.each(properties.methods, method => {
+      var methods = typesByProperty.methods[method] = (typesByProperty.methods[method] || []);
+      methods.push(Type);
+    });
+  };
+
+
+  /**
+   * Finds member accesses and method calls for a given Type
+   */
+  fuzzyioc.parseUsages = Type => {
+
+    var dependencies = {};
+    function initDependency (dep) {
+      dependencies[dep] = {
+        properties: [], // variable
+        functions: []   // { name, args }
+      };
     }
-  });
 
-  _.each(reversedNodes, node => {
-    // console.log(node);
+    var nodes = lookupNodes(Type);
 
-    switch (node.type) {
+    _.map(findDependencies(nodes, Type), initDependency);
 
-      case 'MemberExpression':
-        var object = node.object;
+    _.each(nodes, node => {
 
-        if (object.type == 'Identifier') {
-          var name = object.name;
-          if (name in deps) {
-            deps[name].properties.push(node.property.name);
+      switch (node.type) {
+
+        case 'MemberExpression':
+          var parent = node.parent;
+          var object = node.object;
+
+          var isIdentifier = (object.type == 'Identifier');
+
+          if (isIdentifier) {
+
+            var isParentFunctionCall = (parent.type == 'CallExpression');
+
+            if (!isParentFunctionCall) {
+              var name = object.name;
+              if (name in dependencies) {
+                dependencies[name].properties.push(node.property.name);
+              }
+            }
+            else if (isParentFunctionCall) {
+              var name = object.name;
+              if (name in dependencies) {
+                dependencies[name].functions.push(node.property.name);
+              }
+            }
+          }
+        break;
+      }
+    });
+
+    return dependencies;
+  };
+
+
+
+  /**
+   * Extracts members and methods available on a Type.
+   */
+  function parseProperties (Type) {
+
+    var members = [],
+        methods = [];
+
+    var sourceString = asSourceString(Type);
+    var walk = astw(sourceString);
+
+    walk(node => {
+
+      var object = node.object;
+      var parent = node.parent;
+
+      if (node.type == 'MemberExpression') {
+
+        var isThisExpression = (object.type == 'ThisExpression');
+        var isParentAssignmentExpression = (parent && parent.type == 'AssignmentExpression');
+
+        if (isThisExpression && isParentAssignmentExpression) {
+
+          var isParentRightAssignmentExpressionFunction =
+            (node.parent.right.type == 'FunctionExpression');
+
+          var name = node.property.name;
+          if (isParentRightAssignmentExpressionFunction) {
+            methods.push(name);
+          }
+          else {
+            members.push(name);
           }
         }
+      }
+    });
 
-        // var parent = node.parent;
-        // if (parent.type == 'CallExpression') {
-        //   parent.arguments
-        // }
-      break;
+    return { members, methods };
+  };
+
+  /**
+   * Provides types that satisfy the usage of each dependency.
+   */
+  function satisfyUsages (usages) {
+
+    var satisfiersPerDependency = {};
+    _.each(Object.keys(usages), dependency => {
+      satisfiersPerDependency[dependency] = [];
+    });
+
+    for (var dependency in usages) {
+      var satisfierForDependency = satisfiersPerDependency[dependency];
+
+      var methods = usages[dependency].functions;
+      var members = usages[dependency].properties;
+
+      var methodSatisfiers = _.flatten(_.map(methods, method => {
+        return typesByProperty.methods[method];
+      }));
+
+      var memberSatisfiers = _.flatten(_.map(members, member => {
+        return typesByProperty.members[member];
+      }));
+
+      // find dependencies that satifisfy both methods and members
+      if (methods.length && members.length) {
+        _.each(methodSatisfiers, methodSatisfier => {
+          if (memberSatisfiers.indexOf(methodSatisfier) != -1) {
+            satisfierForDependency.push(methodSatisfier);
+          }
+        });
+      }
+      // find dependencies that satisfy methods
+      else if (methods.length) {
+        _.each(methodSatisfiers, methodSatisfier => satisfierForDependency.push(methodSatisfier));
+      }
+      // find dependencies that satisfy members
+      else if (members.length) {
+        _.each(memberSatisfiers, memberSatisfier => satisfierForDependency.push(memberSatisfier));
+      }
+
+      if (!satisfierForDependency.length) {
+        throw new Error('fuzzyioc: No registered types satisify the usage of your type' +
+                       'Methods called: ' + JSON.stringify(methods) + "\n" +
+                       'Members accessed: ' + JSON.stringify(members) + "\n");
+      }
     }
-  });
 
-  return deps;
+    return satisfiersPerDependency;
+  };
 
-  // nodes.reverse().forEach(function (node) {
-  //   var parent = node.parent;
-  //   if (parent) console.log(parent.type)
-  //   console.log('->', node.type);
-  //   console.log('===');
-  //   console.log(deparse(node));
-  //   console.log('===');
-  //   console.log();
-  //   // var src = deparse(node);
-  //   // console.log(node.type + ' :: ' + JSON.stringify(src));
-  // });
+
+  /**
+   * Looks up a dependencies (arguments) of a FunctionDeclaration or a FunctionExpression
+   */
+  function findDependencies (nodes, Type) {
+    for (var i = 0; i < nodes.length; i++) {
+      var node = nodes[i];
+
+      switch (node.type) {
+
+        // falltrough
+        case 'FunctionDeclaration':
+        case 'FunctionExpression':
+          return _.map(node.params, param => param.name);
+      }
+    }
+
+    throw new Error("fuzzyioc: no function expression or declaration takes dependencies for the type " + Type);
+  };
+
+  /**
+   * Parses type or string to an ast
+   */
+  function lookupNodes (stringOrType) {
+    var sourceString = asSourceString(stringOrType);
+
+    var walk = astw(sourceString);
+
+    var nodes = [];
+    walk(node => nodes.push(node));
+    return nodes.reverse();
+  };
+
+  return fuzzyioc;
+
+};
+
+function asSourceString (input) {
+  return (typeof input == 'function')
+    ? input.toString()
+    : input;
+}
+
+function newInstance (Klass, args) {
+  function F () {}
+  F.prototype = Klass;
+  var instance = new F();
+  Klass.apply(instance, args);
+  return instance;
 }
--- test.js
+++ test.js
@@ -5,23 +5,98 @@ var fuzzyioc = require('./');
 
 describe('fuzzy ioc', function () {
 
-  var SRC = function (argOne, argTwo) {
-    var one = argOne.propOne;
-    var two = argTwo.propTwo;
+  var ioc;
 
-    var resultOne = argOne.funcOne();
-    console.log(argTwo.funcTwo());
-  };
-
-  it('finds names of dependencies', function () {
-    var result = fuzzyioc(SRC);
-    result.should.have.keys('argOne', 'argTwo');
+  beforeEach(function () {
+    ioc = fuzzyioc()
   });
 
-  it('finds accessed properties of dependency', function () {
-    var result = fuzzyioc(SRC);
-    result.argOne.properties.should.contain('propOne');
-    result.argTwo.properties.should.contain('propTwo');
+  describe('lookup', function () {
+
+    it('looks up dependencies by what properties are used', function () {
+
+      function Messages () {
+        this.keys = { 'service.loading': 'Loading new data..' };
+        this.lookup = function (key) {
+          return this.keys[key];
+        };
+      }
+
+      function Service (something) {
+        this.loadingMessage = something.lookup('service.loading');
+      }
+
+      ioc.register(Messages);
+
+      var serviceInstance = ioc(Service);
+
+      serviceInstance.loadingMessage.should.equal('Loading new data..');
+    });
+  });
+
+  describe('register', function () {
+
+    it('registers types', function () {
+      function Type () { }
+      ioc.register(Type);
+      ioc.types().should.contain(Type);
+    });
+  });
+
+  describe('parser', function () {
+
+    var SOURCE = function Source (argOne, argTwo) {
+
+      var local = argOne.propOne;
+      var localCall = argOne.funcOne();
+
+      this.member = argTwo.propTwo;
+      console.log(argTwo.funcTwo());
+
+      this.func = function (nestedArg) {
+        var nestedLocal = argOne.nestedProp;
+        this.setFuncNested = argTwo.nestedFunc();
+      };
+    };
+
+    var dependencies;
+    beforeEach(function () {
+      dependencies = ioc.parseUsages(SOURCE);
+    });
+
+    it('finds names of dependencies', function () {
+      dependencies.should.have.keys('argOne', 'argTwo');
+    });
+
+    it('finds accessed properties of each dependency', function () {
+      dependencies.argOne.properties.should.contain('propOne');
+      dependencies.argTwo.properties.should.contain('propTwo');
+    });
+
+    it('finds accessed functions of each dependency', function () {
+      dependencies.argOne.functions.should.contain('funcOne');
+      dependencies.argTwo.functions.should.contain('funcTwo');
+    });
+
+    it('finds access of nested properties of each dependency', function () {
+      dependencies.argOne.properties.should.contain('nestedProp');
+    });
+
+    it('finds access of nested function calls of each dependency', function () {
+      dependencies.argTwo.functions.should.contain('nestedFunc');
+    });
+
+    it('skips dependencies of nested function definitions', function () {
+      dependencies.should.not.have.keys('nestedArg');
+    });
+
+    it('skips function calls when looking up properties', function () {
+      dependencies.argTwo.properties.should.not.contain('funcTwo');
+    });
+
+    it('skips properties when looking up function calls', function () {
+      dependencies.argOne.functions.should.not.contain('propOne');
+    });
   });
 
 });
